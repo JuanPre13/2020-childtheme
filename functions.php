c4e5e20fb16990486b738fced0265b0722a62f59
juanpre <prendas52@gmail.com>
2022-08-29 16:41:42 -0600

    Child theme footer test

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+    add_action('wp_enqueue_scripts','twenty_twenty_child_theme_equeue_styles' );
+    function twenty_twenty_child_theme_equeue_styles(){
+        wp_enqueue_style( 'twentytwenty-css', get_template_directory_uri().'/style.css' );
+        wp_enqueue_style( 'main.css', '/twentytwenty/child/style.css' );
+    }
+?>
