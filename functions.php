0d2ad9e2f9bdc1a2adbf078306c011bae18229d0
juanpre <prendas52@gmail.com>
2022-09-06 15:28:39 -0600

    Enqueueing parent and child theme styles

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
     add_action('wp_enqueue_scripts','twenty_twenty_child_theme_equeue_styles' );
     function twenty_twenty_child_theme_equeue_styles(){
         wp_enqueue_style( 'twentytwenty-css', get_template_directory_uri().'/style.css' );
-        wp_enqueue_style( 'main.css', '/twentytwenty/child/style.css' );
+        wp_enqueue_style( 'main.css',  get_stylesheet_directory_uri() .'/style.css' );
     }
 ?>
